28f8b82ebb5ef1a2447ab8ee3ba495924e889ca1
sujatagunale <sujata@jsmastery.pro>
2024-09-06 18:43:25 +0530

    readme: update constants

--- README.md
+++ README.md
@@ -639,8 +639,7 @@ export const workExperiences = [
     name: 'Framer',
     pos: 'Lead Web Developer',
     duration: '2022 - Present',
-    title:
-      'Framer serves as my go-to tool for creating interactive prototypes. I use it to bring designs to  life, allowing stakeholders to experience the user flow and interactions before development.',
+    title: "Framer serves as my go-to tool for creating interactive prototypes. I use it to bring designs to  life, allowing stakeholders to experience the user flow and interactions before development.",
     icon: '/assets/framer.svg',
     animation: 'victory',
   },
@@ -649,8 +648,7 @@ export const workExperiences = [
     name: 'Figma',
     pos: 'Web Developer',
     duration: '2020 - 2022',
-    title:
-      'Figma is my collaborative design platform of choice. I utilize it to work seamlessly with team members and clients, facilitating real-time feedback and design iterations. Its cloud-based.',
+    title: "Figma is my collaborative design platform of choice. I utilize it to work seamlessly with team members and clients, facilitating real-time feedback and design iterations. Its cloud-based.",
     icon: '/assets/figma.svg',
     animation: 'clapping',
   },
@@ -659,8 +657,7 @@ export const workExperiences = [
     name: 'Notion',
     pos: 'Junior Web Developer',
     duration: '2019 - 2020',
-    title:
-      'Notion helps me keep my projects organized. I use it for project management, task tracking, and as a central hub for documentation, ensuring that everything from design notes to.',
+    title: "Notion helps me keep my projects organized. I use it for project management, task tracking, and as a central hub for documentation, ensuring that everything from design notes to.",
     icon: '/assets/notion.svg',
     animation: 'salute',
   },
--- src/constants/index.js
+++ src/constants/index.js
@@ -263,8 +263,7 @@ export const workExperiences = [
     name: 'Framer',
     pos: 'Lead Web Developer',
     duration: '2022 - Present',
-    title:
-      'Framer serves as my go-to tool for creating interactive prototypes. I use it to bring designs to  life, allowing stakeholders to experience the user flow and interactions before development.',
+    title: "Framer serves as my go-to tool for creating interactive prototypes. I use it to bring designs to  life, allowing stakeholders to experience the user flow and interactions before development.",
     icon: '/assets/framer.svg',
     animation: 'victory',
   },
@@ -273,8 +272,7 @@ export const workExperiences = [
     name: 'Figma',
     pos: 'Web Developer',
     duration: '2020 - 2022',
-    title:
-      'Figma is my collaborative design platform of choice. I utilize it to work seamlessly with team members and clients, facilitating real-time feedback and design iterations. Its cloud-based.',
+    title: "Figma is my collaborative design platform of choice. I utilize it to work seamlessly with team members and clients, facilitating real-time feedback and design iterations. Its cloud-based.",
     icon: '/assets/figma.svg',
     animation: 'clapping',
   },
@@ -283,8 +281,7 @@ export const workExperiences = [
     name: 'Notion',
     pos: 'Junior Web Developer',
     duration: '2019 - 2020',
-    title:
-      'Notion helps me keep my projects organized. I use it for project management, task tracking, and as a central hub for documentation, ensuring that everything from design notes to.',
+    title: "Notion helps me keep my projects organized. I use it for project management, task tracking, and as a central hub for documentation, ensuring that everything from design notes to.",
     icon: '/assets/notion.svg',
     animation: 'salute',
   },
